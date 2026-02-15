2de75e3393571b3a060b714e6295c9295e8d2f6b
Aditya Anand <contactaditechdev@gmail.com>
2026-02-15 21:08:05 +0530

    Objects typed

---
+++ anotations/objects.ts
@@ -0,0 +1,21 @@
+/** @format */
+
+const profile = {
+	name: "Alex",
+	age: 20,
+	coords: {
+		lat: 0,
+		lng: 15,
+	},
+
+	setAge(age: number): void {
+		this.age = age;
+	},
+};
+
+const { age }: { age: number} = profile;
+const {
+	coords: { lat, lng },
+}: { coords: { lat: number; lng: number } } = profile;
+
+// const {name, age, coords } = profile;
